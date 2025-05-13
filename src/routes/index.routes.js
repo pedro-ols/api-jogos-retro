a397776717b0bb5309e73cbc7bb023658e0dfa47
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-05-13 08:21:18 -0300

    refactor: adjust project

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,6 @@
 import express from 'express'
 
 import authRouter from "./auth.routes.js"
-import animesRouter from "./animeRoutes.js"
-import personagemRouter from "./personagemRoutes.js"
-import collectionRouter from "./collectionRoutes.js"
 import cardRouter from "./cardRoutes.js"
 
 import authMiddleware from '../middleware/authMiddleware.js';
@@ -11,11 +8,7 @@ import authMiddleware from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.use("/auth", authRouter);
-router.use("/colecoes", collectionRouter);
-router.use("/cartas", cardRouter);
 
 router.use(authMiddleware)
-router.use("/animes", animesRouter);
-router.use("/personagens", personagemRouter);
 
 export default router;
